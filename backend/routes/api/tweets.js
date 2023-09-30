13d34909be4db4c80ab0889dd369030df2dc9680
Zi Tan <tanzitian3@gmail.com>
2023-09-30 17:13:20 -0400

    express routing done

--- backend/routes/api/tweets.js
+++ backend/routes/api/tweets.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a User butt resource');
-});
+  res.json({
+    message: "GET /api/tweets"
+  });
+})
 
 module.exports = router;
