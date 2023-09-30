ebbfe31b088daf9ed9292de5cea792cf034f0ce0
Zi Tan <tanzitian3@gmail.com>
2023-09-30 19:11:35 -0400

    login func done

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 require('./models/User');
-
+require('./config/passport'); // <-- ADD THIS LINE
+const passport = require('passport'); 
 const express = require("express");
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+app.use(passport.initialize());
 // Security Middleware
 if (!isProduction) {
   // Enable CORS only in development because React will be on the React
