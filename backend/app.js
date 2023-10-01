b8b674c6fb40bdf9d8beef0bd8147b443ab805e4
Zi Tan <tanzitian3@gmail.com>
2023-09-30 20:04:51 -0400

    user auth section complete

--- backend/app.js
+++ backend/app.js
@@ -1,26 +1,22 @@
-require('./models/User');
-require('./config/passport'); // <-- ADD THIS LINE
-const passport = require('passport'); 
 const express = require("express");
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-
-const cors = require('cors');
-const csurf = require('csurf');
-const { isProduction } = require('./config/keys');
-const debug = require('debug');
-const usersRouter = require('./routes/api/users');
-const tweetsRouter = require('./routes/api/tweets');
-const csrfRouter = require('./routes/api/csrf');
-
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const debug = require("debug");
+require('./models/User');
+const cors = require("cors");
+const csurf = require("csurf");
+const { isProduction } = require("./config/keys");
+const usersRouter = require("./routes/api/users");
+require('./config/passport'); // <-- ADD THIS LINE
+const passport = require('passport'); // <-- ADD THIS LINE
+const tweetsRouter = require("./routes/api/tweets");
+const csrfRouter = require("./routes/api/csrf");
 const app = express();
-
-app.use(logger('dev'));
+app.use(passport.initialize());
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
-app.use(passport.initialize());
 // Security Middleware
 if (!isProduction) {
   // Enable CORS only in development because React will be on the React
@@ -28,7 +24,6 @@ if (!isProduction) {
   // will be served statically on the Express server.)
   app.use(cors());
 }
-
 // Set the _csrf token and create req.csrfToken method to generate a hashed
 // CSRF token
 app.use(
@@ -36,34 +31,30 @@ app.use(
     cookie: {
       secure: isProduction,
       sameSite: isProduction && "Lax",
-      httpOnly: true
-    }
+      httpOnly: true,
+    },
   })
 );
-
 // Attach Express routers
-app.use('/api/users', usersRouter);
-app.use('/api/tweets', tweetsRouter);
-app.use('/api/csrf', csrfRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/tweets", tweetsRouter);
+app.use("/api/csrf", csrfRouter);
 app.use((req, res, next) => {
-    const err = new Error('Not Found');
-    err.statusCode = 404;
-    next(err);
+  const err = new Error("Not Found");
+  err.statusCode = 404;
+  next(err);
+});
+const serverErrorLogger = debug("backend:error");
+// Express custom error handler that will be called whenever a route handler or
+// middleware throws an error or invokes the `next` function with a truthy value
+app.use((err, req, res, next) => {
+  serverErrorLogger(err);
+  const statusCode = err.statusCode || 500;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    statusCode,
+    errors: err.errors,
   });
-  
-  const serverErrorLogger = debug('backend:error');
-  
-  // Express custom error handler that will be called whenever a route handler or
-  // middleware throws an error or invokes the `next` function with a truthy value
-  app.use((err, req, res, next) => {
-    serverErrorLogger(err);
-    const statusCode = err.statusCode || 500;
-    res.status(statusCode);
-    res.json({
-      message: err.message,
-      statusCode,
-      errors: err.errors
-    })
-  });
-  
-  module.exports = app;
+});
+module.exports = app;
--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -2,6 +2,8 @@ const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 const passport = require('passport');
+const { loginUser, restoreUser } = require('../../config/passport');
+const { isProduction } = require('../../config/keys');
 
 var express = require('express');
 var router = express.Router();
@@ -13,20 +15,6 @@ router.get('/', function(req, res, next) {
   });
 })
 
-router.post('/login', async (req, res, next) => {
-  passport.authenticate('local', async function(err, user) {
-    if (err) return next(err);
-    if (!user) {
-      const err = new Error('Invalid credentials');
-      err.statusCode = 400;
-      err.errors = { email: "Invalid credentials" };
-      return next(err);
-    }
-    return res.json({ user });
-  })(req, res, next);
-});
-
-// POST /api/users/register
 router.post('/register', async (req, res, next) => {
   // Check to make sure no one has already registered with the proposed email or
   // username.
@@ -35,7 +23,7 @@ router.post('/register', async (req, res, next) => {
   });
 
   if (user) {
-    // Throw a 400 error if the email address and/or email already exists
+    // Throw a 400 error if the email address and/or username already exists
     const err = new Error("Validation Error");
     err.statusCode = 400;
     const errors = {};
@@ -62,7 +50,7 @@ router.post('/register', async (req, res, next) => {
       try {
         newUser.hashedPassword = hashedPassword;
         const user = await newUser.save();
-        return res.json({ user });
+        return res.json(await loginUser(user)); // <-- THIS IS THE CHANGED LINE
       }
       catch(err) {
         next(err);
@@ -71,4 +59,32 @@ router.post('/register', async (req, res, next) => {
   });
 });
 
+// POST /api/users/login
+router.post('/login', async (req, res, next) => {
+  passport.authenticate('local', async function(err, user) {
+    if (err) return next(err);
+    if (!user) {
+      const err = new Error('Invalid credentials');
+      err.statusCode = 400;
+      err.errors = { email: "Invalid credentials" };
+      return next(err);
+    }
+    return res.json(await loginUser(user)); // <-- THIS IS THE CHANGED LINE
+  })(req, res, next);
+});
+router.get('/current', restoreUser, (req, res) => {
+  if (!isProduction) {
+    // In development, allow React server to gain access to the CSRF token
+    // whenever the current user information is first loaded into the
+    // React application
+    const csrfToken = req.csrfToken();
+    res.cookie("CSRF-TOKEN", csrfToken);
+  }
+  if (!req.user) return res.json(null);
+  res.json({
+    _id: req.user._id,
+    username: req.user.username,
+    email: req.user.email
+  });
+});
 module.exports = router;
